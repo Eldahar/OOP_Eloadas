b58894adb40b71076d1c2b24b3c7c703f5ddc1e6
Oravecz Tibor <oravecz.tibor@edigital.hu>
2015-06-17 12:57:44 +0200

    Init

---
+++ abstract/AbstractDataHandler.php
@@ -0,0 +1,59 @@
+<?php
+
+
+/**
+ * Abstract adatkezelő
+ */
+abstract class AbstractDataHandler {
+    /**
+     * @var string
+     */
+    protected $filename = 'test.txt';
+
+    /**
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * @return array
+     */
+    public function getData() {
+        return $this->data;
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setData(array $data) {
+        $this->data = $data;
+    }
+
+    public function sortData() {
+        sort($this->data);
+    }
+
+    public function save() {
+        foreach($this->data as $dataLine) {
+            $this->writeLine($dataLine);
+        }
+    }
+
+    public function load() {
+        $this->data = $this->loadFile();
+    }
+
+    /**
+     * Írási művelet
+     *
+     * @param string $line
+     */
+    abstract protected function writeLine($line);
+
+    /**
+     * Olvasás fileból
+     *
+     * @return array
+     */
+    abstract protected function loadFile();
+}
---
+++ abstract/FileHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * File kezelő osztály
+ */
+class FileHandler extends AbstractDataHandler {
+    /**
+     * {@inheritdoc}
+     */
+    protected function writeLine($line) {
+        # írási művelet
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function loadFile() {
+        # olvasási művelet
+    }
+
+}
